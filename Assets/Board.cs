3f6e6115c3a5b6327a2177b4d7644228b98413cf
woos-star <dntkd1995@gmail.com>
2020-12-19 16:45:28 +0900

    no message

---
+++ Assets/Board.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    
+    void OnTriggerEnter(Collider other)
+    {
+        (other.Player.tag == "Player")
+            {
+                Debug.Log("Tag=Player");
+                playerdata.money += amount;
+            }
+        }
+
+    void Update()
+    {
+        if 
+        
+    }
+}

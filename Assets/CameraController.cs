1d6e67272615e295f5cc2b1f606efd258680d57e
ozegod <csssecnossing@gmail.com>
2020-12-19 21:23:23 +0900

    スクリプトをカメラとプレイヤーに加えた。 サイコロを回して止めようとするとフリーズする。

---
+++ Assets/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Camera camera1;
+    public Camera camera2;
+
+    void Start()
+    {
+        camera1 = GameObject.Find("Camera1").GetComponent<Camera>();
+        camera2 = GameObject.Find("Camera2").GetComponent<Camera>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        // Call this function to disable FPS camera,
+        // and enable overhead camera.
+        if (TurnManager.currentPlayer == 0)
+        {
+            camera1.enabled = true;
+            camera2.enabled = false;
+        }
+        // Call this function to enable FPS camera,
+        // and disable overhead camera.
+        else
+        {
+            camera1.enabled = false;
+            camera2.enabled = true;
+        }
+    }
+}
--- Assets/Player1Data.cs
+++ Assets/Player1Data.cs
@@ -8,8 +8,8 @@ public class Player1Data : MonoBehaviour
     public static float x;
     public static float y;
     public static float z;
-    public static float angleTheta = 0;
-    public static float anglePhi = 0;
+    public static float angleTheta;
+    public static float anglePhi;
     public static int r = 5;
 
     public Vector3 GetPositionOnSphere(float angleTheta, float anglePhi, int r)
@@ -20,9 +20,16 @@ public class Player1Data : MonoBehaviour
         return new Vector3(x, y, z);
     }
 
+    private void Start()
+    {
+        Vector3 firstposition = GetPositionOnSphere(0f, 0f, 5);
+        transform.position = firstposition;
+    }
+
     void Update()
     {
         GetPositionOnSphere(angleTheta, anglePhi, r);
+        transform.rotation = Quaternion.Euler(0, 0, angleTheta);
     }
 }
 

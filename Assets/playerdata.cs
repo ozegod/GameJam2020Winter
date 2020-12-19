541c4457a9c5226f91ae2361c98fd79ffb5cf914
woos-star <dntkd1995@gmail.com>
2020-12-19 14:38:29 +0900

    Add 3D position(angle)

--- Assets/playerdata.cs
+++ Assets/playerdata.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerdata
+public class PlayerData : MonoBehaviour
 {
     public static int money;
-
     public static float x;
     public static float y;
+    public static float z;
+    public static float angleTheta;
+    public static float anglePhi;
+}
+public class SpherePosition
+{
+    public Vector3 GetPositionOnSphere(float angleTheta, float anglePhi, int r)
+    {
+        float x = r * Mathf.Sin(angleTheta) * Mathf.Cos(anglePhi) ;
+        float y = r * Mathf.Cos(angleTheta)
+        float z = r * Mathf.Sin(angleTheta) * Mathf.Sin(anglePhi)
+        return new Vector3(x, y, z) ;
+    }
 }

385e2eb86eede1db546c52519a881a083326e8fa
ozegod <csssecnossing@gmail.com>
2020-12-19 12:10:46 +0900

    サイコロ機能の実装

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    GameObject Dice;
+    int m = 0;
+    int a = 0;
+
+    private bool moving;
+
+    void RandomDice()
+    {
+        this.Dice.GetComponent<Text>().text =  Random.Range(1, 7).ToString();
+    }
+
+    private void DiceStart()
+    {
+        moving = true;
+    }
+
+    private void DiceStop()
+    {
+        moving = false;
+    }
+
+    void Start()
+    {
+        this.Dice = GameObject.Find("Dice");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            a += 1;
+            if ( a%2 == 0)
+            {
+                DiceStart();
+            }
+            else
+            {
+                DiceStop();
+            }
+        }
+        if (moving)
+        {
+            RandomDice();
+        }
+    }
+}

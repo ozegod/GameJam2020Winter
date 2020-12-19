24202459123443d70cf71c69ce2b951508bcd319
anonymous-gigaflops <canonzou.2010@gmail.com>
2020-12-19 12:15:32 +0900

    データを定義

---
+++ Assets/playerdata.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerdata
+{
+    public static int money;
+
+    public static float x;
+    public static float y;
+}

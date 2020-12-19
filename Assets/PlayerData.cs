142c203cf52659ebb082f915bdbe8b9d67e4c827
ozegod <csssecnossing@gmail.com>
2020-12-19 12:49:26 +0900

    AddPlayerData

---
+++ Assets/PlayerData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+    public static int money;
+    public static float x;
+    public static float y;
+}

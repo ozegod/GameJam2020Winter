7e9c45227f9d7779b36b1ac82acdd5c9ef54457d
ozegod <csssecnossing@gmail.com>
2021-03-11 15:58:40 +0900

    サイコロと移動の仕組みをこのツリーにも加える

--- Assets/Player1Data.cs
+++ Assets/Player1Data.cs
@@ -11,25 +11,29 @@ public class Player1Data : MonoBehaviour
     public static float angleTheta;
     public static float anglePhi;
     public static int r = 5;
+    GameObject player1;
 
     public Vector3 GetPositionOnSphere(float angleTheta, float anglePhi, int r)
     {
-        float x = r * Mathf.Sin(angleTheta) * Mathf.Cos(anglePhi);
-        float y = r * Mathf.Cos(angleTheta);
-        float z = r * Mathf.Sin(angleTheta) * Mathf.Sin(anglePhi);
+        float x = r * Mathf.Sin(angleTheta * 2 * Mathf.PI / 360) * Mathf.Cos(anglePhi * 2 * Mathf.PI / 360);
+        float y = r * Mathf.Cos(angleTheta * 2 * Mathf.PI / 360);
+        float z = r * Mathf.Sin(angleTheta * 2 * Mathf.PI / 360) * Mathf.Sin(anglePhi * 2 * Mathf.PI / 360);
         return new Vector3(x, y, z);
     }
 
     private void Start()
     {
-        Vector3 firstposition = GetPositionOnSphere(0f, 0f, 5);
-        transform.position = firstposition;
+        angleTheta = 0f;
+        anglePhi = 0f;
+        player1 = GameObject.Find("Player1");
+        Vector3 firstposition = GetPositionOnSphere(angleTheta, anglePhi, 5);
+        player1.transform.position = firstposition;
     }
 
     void Update()
     {
-        GetPositionOnSphere(angleTheta, anglePhi, r);
-        transform.rotation = Quaternion.Euler(0, 0, angleTheta);
+        player1.transform.position=GetPositionOnSphere(angleTheta, anglePhi, r);
+        player1.transform.rotation = Quaternion.Euler(0, 0, angleTheta);
     }
 }
 
---
+++ Assetsのコピー（容量のため背景なし）/Player.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    GameObject Dice;
+    private DiceController diceController;
+    private TurnManager turnManager;
+    [SerializeField]
+    // 現在自分のターンかどうか
+    private bool myTurn;
+
+
+    public int d = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // ターンマネジャーを取得
+        turnManager = GameObject.Find("TurnManager").GetComponent<TurnManager>();
+        this.Dice = GameObject.Find("Dice");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 自分のターンでなければなにもしない
+        if (!myTurn)
+        {
+            return;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            diceController.DiceRoll();
+            d = int.Parse(this.Dice.GetComponent<Text>().text);
+            PlayerMovement(d);
+        }
+    }
+
+    public void SetMyTurn(bool flag)
+    {
+        myTurn = flag;
+    }
+
+    void PlayerMovement(int d, int n)
+    {
+        float angleTheta;
+        float anglePhi;
+        if (n == 0)
+        {
+            angleTheta = Player1Data.angleTheta;
+            anglePhi = Player1Data.anglePhi;
+        }
+        else
+        {
+            angleTheta = Player2Data.angleTheta;
+            anglePhi = Player2Data.anglePhi;
+        }
+
+        int step = 0;
+        float angleTheta0 = angleTheta;
+        float anglePhi0 = anglePhi;
+
+        while (step < d*30)
+        {
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                anglePhi += 30f;
+            }
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                anglePhi -= 30f;
+            }
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                angleTheta += 30;
+            }
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                angleTheta -= 30;
+            }
+            PlayerPosition(angleTheta, anglePhi, n);
+            step = (int)Mathf.Abs(angleTheta0 - angleTheta + anglePhi0 - anglePhi);
+            Dice.GetComponent<Text>().text = (d - step).ToString();
+        }
+
+    }
+
+    void PlayerPosition(float angleTheta, float anglePhi, int n)
+    {
+        if (n == 0)
+        {
+            _ = Player1Data.angleTheta == angleTheta;
+            _ = Player1Data.anglePhi == anglePhi;
+        }
+        else
+        {
+            _ = Player2Data.angleTheta == angleTheta;
+            _ = Player2Data.anglePhi == anglePhi;
+        }
+    }
+    
+}

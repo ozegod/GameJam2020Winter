3c5ea550b86750bff752d3f89a192800b91fbf76
csssecnossing@gmail.com <csssecnossing@gmail.com>
2021-03-23 22:03:16 +0900

    「pun2をpullした後、ファイルを削除してcommit試し」を取り消し

---
+++ Assets/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game2");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TurnManager.currentPlayer >= 20)
+        {
+            if (Input.anyKeyDown)
+            {
+                Application.Quit();
+            }
+        }
+        
+    }
+}
